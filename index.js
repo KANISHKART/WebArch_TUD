2397c1037850b16df3c915e6d0adc5832f412050
kanishkar <kanishkrish123@gmail.com>
2023-09-21 18:35:02 +0100

    Dark and light theme

--- index.js
+++ index.js
@@ -8,22 +8,30 @@ navbar.forEach((element) => {
   });
 });
 
-function changeTheme() {
-  console.log(document.documentElement.style.getPropertyValue("--bgColor"))
-}
-
-document.getElementById('change-theme').addEventListener('click',()=>{
-    document.documentElement.style.setProperty('--bgColor', "black")
-    document.documentElement.style.setProperty('--bgSecondaryColor', "black")
-    document.documentElement.style.setProperty('--primaryColor', "white")
-    document.documentElement.style.setProperty('--secondaryColor', "rgb(202, 202, 202)")
-    document.documentElement.style.setProperty('--titleHeader', "lightGreen")
-})
-
-document.getElementById('light-theme').addEventListener('click',()=>{
-    document.documentElement.style.setProperty('--bgColor', "rgb(237, 237, 237)")
-    document.documentElement.style.setProperty('--bgSecondaryColor', "white")
-    document.documentElement.style.setProperty('--primaryColor', "black")
-    document.documentElement.style.setProperty('--secondaryColor', "rgb(105, 105, 105)")
-    document.documentElement.style.setProperty('--titleHeader', "rgb(63, 126, 63)")
-})
+document.getElementById("themeCheckBox").addEventListener("click", () => {
+  if (document.getElementById("themeCheckBox").checked === true) {
+    document.documentElement.style.setProperty("--bgColor", "#161515");
+    document.documentElement.style.setProperty("--bgSecondaryColor", "#161515");
+    document.documentElement.style.setProperty("--primaryColor", "white");
+    document.documentElement.style.setProperty(
+      "--secondaryColor",
+      "rgb(202, 202, 202)"
+    );
+    document.documentElement.style.setProperty("--titleHeader", "lightGreen");
+  } else {
+    document.documentElement.style.setProperty(
+      "--bgColor",
+      "rgb(237, 237, 237)"
+    );
+    document.documentElement.style.setProperty("--bgSecondaryColor", "white");
+    document.documentElement.style.setProperty("--primaryColor", "black");
+    document.documentElement.style.setProperty(
+      "--secondaryColor",
+      "rgb(105, 105, 105)"
+    );
+    document.documentElement.style.setProperty(
+      "--titleHeader",
+      "rgb(63, 126, 63)"
+    );
+  }
+});
